dad47ac5bdd34c18214dee47be7f9ee2cd3a9b3f
Stefan Penner <stefan.penner@gmail.com>
2015-01-04 20:28:25 -0500

    * proper test setup (no more delegating to a cli) * test for configuration (inline vs non-inline source map) * update links package.json

--- fixtures/expected.js
+++ fixtures/expected.js
@@ -1,5 +1,3 @@
-(function() {
-  var x = 0;
-})();
+"use strict";
 
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZml4dHVyZXMuanMiLCJzb3VyY2VzIjpbImZpeHR1cmVzLmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7TUFBTSxJQUFJIiwic291cmNlc0NvbnRlbnQiOlsiY29uc3QgeCA9IDA7XG4iXX0=
+var x = 0;
--- test.js
+++ test.js
@@ -1,17 +1,46 @@
 'use strict';
 
 var fs     = require('fs');
-var assert = require('assert');
-var rimraf = require('rimraf');
+var expect = require('chai').expect;
+var broccoli = require('broccoli');
+var path = require('path');
+var to5 = require('./index');
 
-/* global afterEach: false */
+var builder;
+var inputPath = path.join(__dirname, 'fixtures');
+
 afterEach(function () {
-  rimraf.sync('temp');
+  builder.cleanup();
 });
 
-it('transpile ES6 to ES5', function () {
-  assert.equal(
-    fs.readFileSync('temp/fixtures.js',     'utf8'),
-    fs.readFileSync('fixtures/expected.js', 'utf8')
-  );
+function build(path, options) {
+  builder = new broccoli.Builder(to5(path, options));
+
+  return builder.build();
+}
+
+describe('transpile ES6 to ES5', function() {
+  it('basic', function () {
+    return build(inputPath, {}).then(function(results) {
+      var outputPath = results.directory;
+
+      var output = fs.readFileSync(path.join(outputPath, 'fixtures.js')).toString();
+      var input = fs.readFileSync(path.join(inputPath,  'expected.js')).toString();
+
+      expect(output).to.eql(input);
+    });
+  });
+
+  it('inline source maps', function () {
+    return build(inputPath, {
+      sourceMap: 'inline'
+    }).then(function(results) {
+      var outputPath = results.directory;
+
+      var output = fs.readFileSync(path.join(outputPath, 'fixtures.js')).toString();
+      var input = fs.readFileSync(path.join(inputPath,  'expected-inline-source-maps.js')).toString();
+
+      expect(output).to.eql(input);
+    });
+  });
 });
