17fa4da707e61c3a180a24cb24778f8a6ebda032
Stefan Penner <stefan.penner@gmail.com>
2015-01-12 13:04:03 -0500

    improve transpiler name (for logging/traces)

--- index.js
+++ index.js
@@ -3,22 +3,22 @@
 var transpiler = require('6to5-core');
 var Filter     = require('broccoli-filter');
 
-function TranspilerFilter(inputTree, options) {
-  if (!(this instanceof TranspilerFilter)) {
-    return new TranspilerFilter(inputTree, options);
+function SixToFive(inputTree, options) {
+  if (!(this instanceof SixToFive)) {
+    return new SixToFive(inputTree, options);
   }
 
   this.inputTree = inputTree;
   this.options = options || {};
 }
 
-TranspilerFilter.prototype = Object.create(Filter.prototype);
-TranspilerFilter.prototype.constructor = TranspilerFilter;
+SixToFive.prototype = Object.create(Filter.prototype);
+SixToFive.prototype.constructor = SixToFive;
 
-TranspilerFilter.prototype.extensions = ['js'];
-TranspilerFilter.prototype.targetExtension = 'js';
+SixToFive.prototype.extensions = ['js'];
+SixToFive.prototype.targetExtension = 'js';
 
-TranspilerFilter.prototype.processString = function (string, relativePath) {
+SixToFive.prototype.processString = function (string, relativePath) {
   var opts = this.copyOptions();
 
   opts.filename = opts.sourceMapName = opts.sourceFileName = relativePath;
@@ -26,8 +26,8 @@ TranspilerFilter.prototype.processString = function (string, relativePath) {
   return transpiler.transform(string, opts).code;
 };
 
-TranspilerFilter.prototype.copyOptions = function() {
+SixToFive.prototype.copyOptions = function() {
   return JSON.parse(JSON.stringify(this.options));
 };
 
-module.exports = TranspilerFilter;
+module.exports = SixToFive;
