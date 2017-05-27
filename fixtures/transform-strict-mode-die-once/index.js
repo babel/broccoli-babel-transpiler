2c0e956e35ea6b53213647c29396a2842e01daf7
Michael Stewart <mistewart@linkedin.com>
2017-05-26 17:25:06 -0700

    test worker termination

---
+++ fixtures/transform-strict-mode-die-once/index.js
@@ -0,0 +1,24 @@
+"use strict";
+
+var fs = require('fs');
+var path = require('path');
+var pluginFunction = require('babel-plugin-transform-strict-mode');
+
+module.exports = {
+  name: 'transform-strict-mode',
+
+  build: function(options) {
+    if (fs.existsSync(options.ripFile)) {
+      // already died once
+      fs.unlinkSync(options.ripFile);
+      return pluginFunction;
+    }
+    else {
+      // haven't died yet
+      fs.writeFileSync(options.ripFile, 'I lived a short but intense life');
+      return function() {
+        process.exit(1);
+      };
+    }
+  }
+};
