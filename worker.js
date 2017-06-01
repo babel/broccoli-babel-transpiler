97e4ea599d7f91cee60544f3dbb91d0017a026ae
Michael Stewart <mistewart@linkedin.com>
2017-06-01 12:00:42 -0700

    handle case where new API is used in main process

--- worker.js
+++ worker.js
@@ -3,33 +3,14 @@
 var transpiler = require('babel-core');
 var workerpool = require('workerpool');
 var Promise = require('rsvp').Promise;
+var transformOptions = require('./transform-options');
 
 
 // transpile the input string, using the input options
 function transform(string, options) {
 
-  if (options.resolveModuleSource !== undefined) {
-    // convert to work with Babel, if needed
-    if (Object.prototype.toString.call(options.resolveModuleSource) === '[object Array]') {
-      options.resolveModuleSource = require(options.resolveModuleSource[1]).build(options.resolveModuleSource[2]);
-    }
-  }
-
-  if (options.plugins !== undefined) {
-    // convert plugins to work with Babel, if needed
-    options.plugins = options.plugins.map(function(plugin) {
-      if (Object.prototype.toString.call(plugin) === '[object Array]') {
-        return require(plugin[1]).build(plugin[2]);
-      }
-      else {
-        // plugin is a string, that's fine
-        return plugin;
-      }
-    });
-  }
-
   return new Promise(function(resolve) {
-    var result = transpiler.transform(string, options);
+    var result = transpiler.transform(string, transformOptions(options));
     // this is large, not used, and can't be serialized anyway
     delete result.ast;
 
