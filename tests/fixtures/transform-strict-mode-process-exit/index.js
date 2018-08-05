0dfc873d8883091526624e12cb38267130df3b8b
Dan Freeman <dfreeman@salsify.com>
2018-08-05 16:22:03 -0400

    Get tests passing? Let's see what CI says

--- tests/fixtures/transform-strict-mode-process-exit/index.js
+++ tests/fixtures/transform-strict-mode-process-exit/index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var pluginFunction = require('babel-plugin-transform-strict-mode');
+var pluginFunction = require('@babel/plugin-transform-strict-mode');
 
 module.exports = {
   name: 'transform-strict-mode',
